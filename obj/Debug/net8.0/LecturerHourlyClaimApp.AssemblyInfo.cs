24059185a0c7a795b75bdebc8d087dd5736a9354
Christo Mienie <128799213+ChrisV2V2@users.noreply.github.com>
2024-10-15 20:15:45 +0200

    Added CSS to all Views

--- obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7784c0b099008341cf9518665b2824936c10c011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b1b8d8decc15e0798ed6febae2f6edec369c3e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
