ceec935d4ef9823902c51d721e6b669eab11a3ee
Christo Mienie <128799213+ChrisV2V2@users.noreply.github.com>
2024-10-16 15:58:25 +0200

    Fixed issues with xUnit Test where LecturerHourlyClaimApp.Tests used older version than the main project

--- obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1773a492c999e2a773cb4fe4af70a752476f1509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922602a5f6330c9eacc04df959534bf4cbf50f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
