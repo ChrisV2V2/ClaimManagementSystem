1773a492c999e2a773cb4fe4af70a752476f1509
Christo Mienie <128799213+ChrisV2V2@users.noreply.github.com>
2024-10-16 11:17:07 +0200

    Added animation and header to login page

--- obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b1b8d8decc15e0798ed6febae2f6edec369c3e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce6a85834cd66f145e796ac10b52be491f3f1f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
