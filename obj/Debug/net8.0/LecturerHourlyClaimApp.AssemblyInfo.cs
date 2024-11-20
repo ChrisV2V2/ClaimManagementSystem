8362a3576fc526c15e1bf2e6e39cb85d4d7d9003
Christo Mienie <128799213+ChrisV2V2@users.noreply.github.com>
2024-11-20 07:22:29 +0200

    Added an option for exporting all approved claims from the View Approved Claims menu

--- obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LecturerHourlyClaimApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602e1af16a5abef3046f63918879d5a47b95be9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8652486b8c7a084b29b94d9fbb28a77982c1c063")]
 [assembly: System.Reflection.AssemblyProductAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LecturerHourlyClaimApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
